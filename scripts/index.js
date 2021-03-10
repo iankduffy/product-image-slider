f6097c20dfa50fb9a5d11b0bd9c121eedd3011e8
Ian Duffy <ian.duffy@shiftcommerce.com>
2021-03-10 21:20:38 +0000

    add slider counter

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const slider = document.querySelector('.slides')
 const rightArrow = document.querySelector('.right.arrow')
 const leftArrow = document.querySelector('.left.arrow')
 const slides = Array.from(slider.children)
+const dotsContainer = document.querySelector('.slide-dots')
 
 const getCurrentSlideWidth = () => {
   return slides.length > 0 ? slides[0].offsetWidth : 500
@@ -25,9 +26,70 @@ leftArrow.addEventListener('click', () => {
 
 const getVisibleSlideIndex = () => {
   const scrollLeft = slider.scrollLeft
+  const allSlide = Array.from(slider.children)
+  console.log(scrollLeft)
 
   return slides.findIndex((slide) => slide.offsetLeft === scrollLeft)
 }
+
+getVisibleSlideIndex()
+
+const addSlideDots = () => {
+  slides.forEach((slide) => {
+    let li = document.createElement('li')
+    li.classList = 'slide-dot'
+    dotsContainer.appendChild(li)
+  })
+  dotsContainer.children[0].classList.add('active')
+}
+
+addSlideDots()
+
+const updateSlideNumber = () => {
+  const currentSlide = getVisibleSlideIndex()
+  const dots = dotsContainer.children
+
+  Array.from(dots).forEach((slide) => {
+    slide.classList.remove('active')
+  })
+
+  dots[currentSlide].classList.add('active')
+}
+
+function debounce (func, wait, immediate) {
+  var timeout
+  return function debouncedFunction () {
+    var context = this
+    var args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
+// function throttle(func, timeFrame) {
+//   var lastTime = 0;
+//   return function (...args) {
+//       var now = new Date();
+//       if (now - lastTime >= timeFrame) {
+//           func(...args);
+//           lastTime = now;
+//       }
+//   };
+// }
+
+slider.addEventListener('scroll', debounce(updateSlideNumber, 100));
+
+// slider.addEventListener('scroll', updateSlideNumber);
+
+
+
+
 
 // const callback = (entries, observer) => {
 //   entries.forEach(entry => {
