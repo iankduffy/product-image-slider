1c291331926dd4746cd3115c0dbd63ad66e4d051
Ian Duffy <ian.duffy@shiftcommerce.com>
2021-03-11 19:57:41 +0000

    hero slider - product slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,87 +1,3 @@
-const slider = document.querySelector('.slides')
-const rightArrow = document.querySelector('.right.arrow')
-const leftArrow = document.querySelector('.left.arrow')
-const slides = Array.from(slider.children)
-const dotsContainer = document.querySelector('.slide-dots')
-
-const getCurrentSlideWidth = () => {
-  return slides.length > 0 ? slides[0].offsetWidth : 500
-}
-
-const getVisibleSlideIndex = () => {
-  const scrollLeft = slider.scrollLeft
-
-  return slides.findIndex((slide) => slide.offsetLeft === scrollLeft)
-}
-
-const lastSlide = () => {
-  const index = getVisibleSlideIndex()
-  return index == slides.length - 1
-}
-
-const firstSlide = () => {
-  const index = getVisibleSlideIndex()
-  return index == 0
-}
-
-const scrollLeftByOne = () => {
-  const lastSlide = slides.length - 1
-  if (firstSlide()) {
-    scrollToSlideAtIndex(lastSlide)
-  } else {
-    slider.scrollLeft -= getCurrentSlideWidth()
-  }
-}
-
-const scrollRightByOne = () => {
-  if (lastSlide()) {
-    scrollToSlideAtIndex(0)
-  } else {
-    slider.scrollLeft += getCurrentSlideWidth()
-  }
-}
-
-rightArrow.addEventListener('click', () => {
-  scrollRightByOne()
-})
-
-leftArrow.addEventListener('click', () => {
-  scrollLeftByOne()
-})
-
-getVisibleSlideIndex()
-
-const scrollToSlideAtIndex = (selectedIndex) => {
-  slider.scrollLeft = selectedIndex * getCurrentSlideWidth()
-}
-
-const addSlideDots = () => {
-  slides.forEach((slide) => {
-    let li = document.createElement('li')
-    li.classList = 'slide-dot'
-    dotsContainer.appendChild(li)
-  })
-  dotsContainer.children[0].classList.add('active')
-}
-
-addSlideDots()
-
-const updateSlideNumber = () => {
-  const currentSlide = getVisibleSlideIndex()
-  const dots = dotsContainer.children
-
-  Array.from(dots).forEach((dot) => {
-    dot.classList.remove('active')
-  })
-
-  slides.forEach(slide => {
-    slide.classList.remove('active')
-  })
-
-  slides[currentSlide]?.classList.add('active')
-  dots[currentSlide]?.classList.add('active')
-}
-
 function debounce (func, wait, immediate) {
   var timeout
   return function debouncedFunction () {
@@ -98,6 +14,102 @@ function debounce (func, wait, immediate) {
   }
 }
 
+const shiftSlider = (element, opts = {}) => {
+  const sliderContainer = element 
+  const slider = sliderContainer.querySelector('.slides')
+  const slides = Array.from(slider.children)
+  
+  const getCurrentSlideWidth = () => {
+    return slides.length > 0 ? slides[0].offsetWidth : 500
+  }
+  
+  const getVisibleSlideIndex = () => {
+    const scrollLeft = slider.scrollLeft
+  
+    return slides.findIndex((slide) => slide.offsetLeft === scrollLeft)
+  }
+  
+  const lastSlide = () => {
+    const index = getVisibleSlideIndex()
+    return index == slides.length - 1
+  }
+  
+  const firstSlide = () => {
+    const index = getVisibleSlideIndex()
+    return index == 0
+  }
+
+  const scrollToSlideAtIndex = (selectedIndex) => {
+    slider.scrollLeft = selectedIndex * getCurrentSlideWidth()
+  }
+
+  const scrollLeftByOne = () => {
+    const lastSlide = slides.length - 1
+    if (firstSlide()) {
+      scrollToSlideAtIndex(lastSlide)
+    } else {
+      slider.scrollLeft -= getCurrentSlideWidth()
+    }
+  }
+  
+  const scrollRightByOne = () => {
+    if (lastSlide()) {
+      scrollToSlideAtIndex(0)
+    } else {
+      slider.scrollLeft += getCurrentSlideWidth()
+    }
+  }
+
+
+  // Arrows
+  const rightArrow = sliderContainer.querySelector('.right.arrow')
+  const leftArrow = sliderContainer.querySelector('.left.arrow')
+  if (rightArrow) {
+    rightArrow.addEventListener('click', () => {
+      scrollRightByOne()
+    })
+  }
+
+  if (leftArrow) {
+    leftArrow.addEventListener('click', () => {
+      scrollLeftByOne()
+    })
+  }
+
+  // Dots Container
+  const dotsContainer = sliderContainer.querySelector('.slide-dots')
+  if (dotsContainer) {
+    slides.forEach((slide) => {
+    let li = document.createElement('li')
+      li.classList = 'slide-dot'
+      dotsContainer.appendChild(li)
+    })
+    dotsContainer.children[0].classList.add('active')
+
+    const updateSlideNumber = () => {
+      const currentSlide = getVisibleSlideIndex()
+      const dots = dotsContainer.children
+    
+      Array.from(dots).forEach((dot) => {
+        dot.classList.remove('active')
+      })
+    
+      slides.forEach(slide => {
+        slide.classList.remove('active')
+      })
+    
+      slides[currentSlide]?.classList.add('active')
+      dots[currentSlide]?.classList.add('active')
+    }
+
+    slider.addEventListener('scroll', debounce(updateSlideNumber, 100));
+  }
+}
+
+const shiftSliders = document.querySelectorAll('.shift-slider-wrapper')
+
+shiftSliders.forEach(slider => shiftSlider(slider))
+
 // function throttle(func, timeFrame) {
 //   var lastTime = 0;
 //   return function (...args) {
@@ -109,13 +121,11 @@ function debounce (func, wait, immediate) {
 //   };
 // }
 
-slider.addEventListener('scroll', debounce(updateSlideNumber, 100));
+// document.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('slide-dot')) {
+//     const dots = Array.from(dotsContainer.children)
+//     const index = dots.findIndex(dot => dot == e.target)
 
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('slide-dot')) {
-    const dots = Array.from(dotsContainer.children)
-    const index = dots.findIndex(dot => dot == e.target)
-
-    scrollToSlideAtIndex(index)
-  }
-})
+//     scrollToSlideAtIndex(index)
+//   }
+// })
